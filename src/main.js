70a344f49011731e69cf629c664392cd4bc4a64f
zhaoliang <kybetter@163.com>
2019-10-31 21:35:31 +0800

    feat: add main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+let port = document.getElementsByTagName('meta')['port']
+port = port ? port.getAttribute('content') : 12345;
+
+Vue.prototype.$http = axios.create({
+  baseURL: `//localhost:${port}/`,
+  timeout: 10000,
+});;
 
 new Vue({
   router,
