b21be6596cc4d3efc01175b3b849782800f4749f
zhaoliang <kybetter@163.com>
2019-12-10 21:14:09 +0800

    feat: 增加 mockJS 语法支持

--- server/custom-router/router.js
+++ server/custom-router/router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Mock = require('mockjs');
 
 module.exports = async function setCustomApi() {
   // make sure old api is not avaliable.
@@ -7,7 +8,11 @@ module.exports = async function setCustomApi() {
     const apiList = await normalApiDb.allDocs({ include_docs: true });
     apiList.rows.forEach(item => {
       router[item.doc.method.toLowerCase()](item.doc.api, (req, res) => {
-        res.send(item.doc.content);
+        if (item.doc.content.indexOf('<') === 0 || item.doc.content.indexOf('//') !== -1) {
+          res.send(Mock.mock(item.doc.content));
+        } else {
+          res.send(Mock.mock(JSON.parse(item.doc.content)));
+        }
       })
     })
   } catch(e) {
